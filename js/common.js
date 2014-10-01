f973d7d5d151adddf4026c7e26e08358ecb16795
etna-scorpio <etna.scorpio@gmail.com>
2014-10-01 17:58:00 +0300

    hosting is done

--- js/common.js
+++ js/common.js
@@ -5,4 +5,17 @@ head.ready(function() {
 		step: 6.730752
 	});
 
+	$('.features__table').jScrollPane({
+		verticalDragMaxHeight: 30
+	});
+
+	$('.butn').click(function() {
+		if($(this).hasClass('is-active')){
+			$(this).removeClass('is-active');
+			$(this).siblings().addClass('is-active');
+		} else {
+			$(this).addClass('is-active');
+			$(this).siblings().removeClass('is-active');
+		}
+	});
 });
--- js/init.js
+++ js/init.js
@@ -2,4 +2,6 @@ head.load("js/lib/jquery.js",
 					"js/lib/cycle.js",
 					"js/lib/cycle.carousel.js",
 					"js/lib/jquery-ui.min.js",
+					"js/lib/jScrollPane.min.js",
+					"js/lib/jquery.mousewheel.js",
 					"js/common.js");
